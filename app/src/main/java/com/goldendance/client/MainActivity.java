1c0525057a365211f8ef68219e1ea9155ec72c17
zhuanghm <zhuanghm@vastio.com>
2016-12-11 13:29:14 +0800

    update gitignore

--- app/src/main/java/com/goldendance/client/MainActivity.java
+++ app/src/main/java/com/goldendance/client/MainActivity.java
@@ -7,6 +7,10 @@ import com.goldendance.client.base.BaseActivity;
 
 import com.goldendance.client.utils.LogUtils;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
 public class MainActivity extends BaseActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -14,13 +18,35 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        EventBus.getDefault().register(this);
         findViewById(R.id.btnCrash).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LogUtils.i(TAG, "" + 1 / 0);
+//                LogUtils.i(TAG, "" + 1 / 0);
+                EventBus.getDefault().post(new MessageEvent("aaa", "aaa"));
             }
         });
+    }
 
+    public class MessageEvent {
+        public final String name;
+        public final String password;
+
+        public MessageEvent(String name, String password) {
+            this.name = name;
+            this.password = password;
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void helloEventBus(MessageEvent message) {
+        LogUtils.d(TAG, message.name);
+    }
+
+    @Override
+    protected void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
     }
 }
